1926363eb6f24c7caeff33320a2b785c1b53b9e9
Hozi7 <Hossanaorho@gmail.com>
2024-11-26 11:24:12 +0100

    List compression

--- New codes.py
+++ New codes.py
@@ -1,7 +1,13 @@
-# List compressions
+# List Compression
 
 numbers = [1, 2, 3, 4, 5]
 
 numbers_power_2 = [n**2 for n in numbers]
 
 print(numbers_power_2)
+
+numbers_power_2 = []
+for n in numbers:
+    numbers_power_2.append(n**2)
+
+print("number_power_2")
